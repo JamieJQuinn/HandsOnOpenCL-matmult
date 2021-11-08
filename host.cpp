77c3a42a4309adf7fe24bf1cf26051d147ef9d1d
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-11-08 16:06:29 +0000

    Add comments for clarity

--- host.cpp
+++ host.cpp
@@ -15,21 +15,25 @@ int setDefaultPlatform(const std::string& targetName) {
   std::vector<cl::Platform> platforms;
   cl::Platform::get(&platforms);
   cl::Platform plat;
+
+  // Print out names of all available platforms
   std::cout << "Available platforms:" << std::endl;
   for (auto &p : platforms) {
     std::string platname = p.getInfo<CL_PLATFORM_NAME>();
     std::cout << platname << std::endl;
   }
 
+  // Find first platform matching name
   for (auto &p : platforms) {
     std::string platname = p.getInfo<CL_PLATFORM_NAME>();
     if (platname.find(targetName) != std::string::npos) {
       plat = p;
+      break;
     }
   }
 
   if (plat() == 0)  {
-    std::cout << "No OpenCL 2.0 platform found.\n";
+    std::cout << "No platform found with name " << targetName << ".\n";
     return -1;
   }
 
@@ -46,6 +50,7 @@ int setDefaultPlatform(const std::string& targetName) {
 }
 
 auto read_file(std::string_view path) -> std::string {
+  // Read entire file into string
   // stolen from https://stackoverflow.com/a/116220
   constexpr auto read_size = std::size_t{4096};
   auto stream = std::ifstream{path.data()};
@@ -61,6 +66,7 @@ auto read_file(std::string_view path) -> std::string {
 }
 
 cl::Program buildProgram(const std::string& filename) {
+  // Compile kernel source into program
   cl::Program program(read_file(filename), false);
   try {
     program.build("-cl-std=CL2.0");
@@ -74,10 +80,12 @@ cl::Program buildProgram(const std::string& filename) {
 }
 
 inline int idx(int i, int j, int N) {
+  // Correctly calculate the global index 100% of the time, every time
   return i*N+j;
 }
 
 void mat_mult(int N, float *A, float *B, float *C) {
+  // Basic CPU implementation
   for (int i=0; i<N; ++i) {
     for (int j=0; j<N; ++j) {
       float tmp=0.0;
@@ -91,6 +99,7 @@ void mat_mult(int N, float *A, float *B, float *C) {
 }
 
 void mat_mult_omp(int N, float *A, float *B, float *C) {
+  // Basic OpenMP implementation
 #pragma omp parallel for collapse(2)
   for (int i=0; i<N; ++i) {
     for (int j=0; j<N; ++j) {
@@ -105,13 +114,15 @@ void mat_mult_omp(int N, float *A, float *B, float *C) {
 }
 
 void load_random_data(std::vector<float>& A_in, std::vector<float>& B_in) {
+  // Fill the matrices with essentially random integers (distributed around 0)
   for(int i=0; i<A_in.size(); ++i) {
-    A_in[i] = i%10-5;
-    B_in[i] = (i-5)%10-5;
+    A_in[i] = i%11-5;
+    B_in[i] = (i-5)%11-5;
   }
 }
 
 void load_test_case(std::vector<float>& A_in, std::vector<float>& B_in) {
+  // Load specific test data
   float A[] = {
     5,  6,  3,
     7,  2, -2,
@@ -131,6 +142,7 @@ void load_test_case(std::vector<float>& A_in, std::vector<float>& B_in) {
 }
 
 void check_test_result(std::vector<float>& C_in) {
+  // Check input against known result
   float C[] = {
     -3, 42, 62,
     -11, 2, 5,
@@ -143,6 +155,7 @@ void check_test_result(std::vector<float>& C_in) {
 }
 
 void verify_seq() {
+  // Make sure basic CPU implementation works
   const int N = 3;
 
   std::vector<float> h_A(N*N), h_B(N*N), h_C(N*N);
@@ -159,6 +172,7 @@ void run_seq(
     std::vector<float>& h_B,
     std::vector<float>& h_C)
 {
+  // Time sequential
   auto start = high_resolution_clock::now();
 
   mat_mult(N, h_A.data(), h_B.data(), h_C.data());
@@ -175,6 +189,7 @@ void run_seq_omp(
     std::vector<float>& h_B,
     std::vector<float>& h_C)
 {
+  // Time OpenMP
   auto start = high_resolution_clock::now();
 
   mat_mult_omp(N, h_A.data(), h_B.data(), h_C.data());
@@ -186,24 +201,31 @@ void run_seq_omp(
 }
 
 void verify_ocl(const std::string& kernelName) {
+  // Ensure the basic OpenCL version works
   const int N = 3;
 
+  // Host mem
   std::vector<float> h_A(N*N), h_B(N*N), h_C(N*N);
   load_test_case(h_A, h_B);
 
+  // Device mem
   cl::Buffer d_A = cl::Buffer(h_A.begin(), h_A.end(), true);
   cl::Buffer d_B = cl::Buffer(h_B.begin(), h_B.end(), true);
   cl::Buffer d_C = cl::Buffer(CL_MEM_READ_WRITE, sizeof(float)*h_C.size());
 
+  // Load program & extract kernel named kernelName
   cl::Program program = buildProgram("mat_mult.cl");
   auto mat_mult_cl = cl::KernelFunctor<
     int, cl::Buffer, cl::Buffer, cl::Buffer
     >(program, kernelName);
 
+  // run kernel
   mat_mult_cl(cl::EnqueueArgs(cl::NDRange(N, N)), N, d_A, d_B, d_C);
 
+  // Get data back
   cl::copy(d_C, h_C.begin(), h_C.end());
 
+  // Check result
   check_test_result(h_C);
 }
 
@@ -214,6 +236,7 @@ void run_ocl(
     std::vector<float>& h_B,
     std::vector<float>& h_C)
 {
+  // Time basic OpenCL
   cl::Buffer d_A = cl::Buffer(h_A.begin(), h_A.end(), true);
   cl::Buffer d_B = cl::Buffer(h_B.begin(), h_B.end(), true);
   cl::Buffer d_C = cl::Buffer(CL_MEM_READ_WRITE, sizeof(float)*h_C.size());
@@ -252,7 +275,7 @@ void run_1d_ocl(
     >(program, kernelName);
 
   auto start = high_resolution_clock::now();
-
+  // Run over 1 dimension, use workgroups of N/16
   mat_mult_cl(cl::EnqueueArgs(cl::NDRange(N), cl::NDRange(N/16)), N, d_A, d_B, d_C);
 
   cl::copy(d_C, h_C.begin(), h_C.end());
@@ -273,15 +296,19 @@ void run_1d_wrk_ocl(
   cl::Buffer d_A = cl::Buffer(h_A.begin(), h_A.end(), true);
   cl::Buffer d_B = cl::Buffer(h_B.begin(), h_B.end(), true);
   cl::Buffer d_C = cl::Buffer(CL_MEM_READ_WRITE, sizeof(float)*h_C.size());
+
+  // Allocate temp space local to workgroups
   cl::LocalSpaceArg d_wrk = cl::Local(sizeof(float)*N);
 
   cl::Program program = buildProgram("mat_mult.cl");
+  // Build kernel with extra workgroup space
   auto mat_mult_cl = cl::KernelFunctor<
     int, cl::Buffer, cl::Buffer, cl::Buffer, cl::LocalSpaceArg
     >(program, kernelName);
 
   auto start = high_resolution_clock::now();
 
+  // Pass in workgroup
   mat_mult_cl(cl::EnqueueArgs(cl::NDRange(N), cl::NDRange(N/4)), N, d_A, d_B, d_C, d_wrk);
 
   cl::copy(d_C, h_C.begin(), h_C.end());
@@ -335,6 +362,7 @@ int main() {
     run_1d_ocl("mat_mult_1d", N, h_A, h_B, h_C);
     check_equal(h_C, h_C_seq);
 
+    // this version does weird things, probably shouldn't bother
     //std::cout << "1D cc:  ";
     //run_1d_wrk_ocl("mat_mult_1d_col_copy", N, h_A, h_B, h_C);
     //check_equal(h_C, h_C_seq);
